06a715fb158d5d08f291e60f973119aa29804898
liutong19890905 <tongliu@deepglint.com>
2016-04-13 17:57:58 +0800

    update

--- caffe/src/caffe/net.cpp
+++ caffe/src/caffe/net.cpp
@@ -724,7 +724,7 @@ void Net<Dtype>::CopyTrainedLayersFrom(const string trained_filename) {
   fread(buffer, 1, size, fp);
   fclose(fp);
 
-  ReadNetParamsFromMemoryOrDie(trained_filename, buffer, size, &param)
+  ReadNetParamsFromMemoryOrDie(trained_filename, buffer, size, &param);
 
   printf("%s\n", "++++++++++++++++++++");
   printf("%d\n", size);
