c74901c2ff0a6b2b60ef5c99067fa37fdd17260b
liutong19890905 <tongliu@deepglint.com>
2016-04-18 11:04:26 +0800

    add is_encrypt

--- caffe/src/caffe/net.cpp
+++ caffe/src/caffe/net.cpp
@@ -43,7 +43,31 @@ template <typename Dtype>
 Net<Dtype>::Net(const string& param_file, Phase phase, bool is_encrypt) {
   is_encrypt_ = is_encrypt;
   NetParameter param;
-  ReadNetParamsFromTextFileOrDie(param_file, &param);
+  if (is_encrypt == true)
+  {
+    FILE *fp = fopen(param_file.c_str(), "rb");
+    fseek(fp, 0L, SEEK_END);
+    long int size = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+
+    unsigned char *buffer = (unsigned char *)malloc(size);
+    fread(buffer, size, 1, fp);
+    fclose(fp);
+
+    unsigned char *decrypt = (unsigned char *) malloc(size);
+
+    DecryptModel(buffer, size, decrypt);
+
+    LOG(INFO) << "Caffe will load AES model, size=" << size;
+    ReadNetParamsFromMemoryOrDie(param_file, decrypt, size, &param);
+
+    free(buffer);
+    free(decrypt);
+  }
+  else
+  {
+    ReadNetParamsFromTextFileOrDie(param_file, &param);
+  }
   param.mutable_state()->set_phase(phase);
   Init(param);
 }
