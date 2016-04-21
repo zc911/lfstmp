cec46fd030bc85a186a36d7d3277152a08c6cd19
zhenchen <zhenchen@deepglint.com>
2016-04-21 15:16:35 +0800

    init glog

--- apps/src/witness_main.cpp
+++ apps/src/witness_main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <memory>
 #include <iostream>
+#include <glog/logging.h>
 #include "config/config.h"
 #include "service/witness_service.h"
 #include "service/witness_service_asyn.h"
@@ -10,6 +11,8 @@ using namespace std;
 using namespace dg;
 
 int main(int argc, char* argv[]) {
+
+    google::InitGoogleLogging(argv[0]);
 
     Config *config = Config::GetInstance();
     config->Load("config.json");
