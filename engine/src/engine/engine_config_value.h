60c941e50285a025764a8aef7ae6ad2aafb7a492
zhenchen <zhenchen@deepglint.com>
2016-04-26 09:49:57 +0800

    engine with both vehicle and face

--- engine/src/engine/engine_config_value.h
+++ engine/src/engine/engine_config_value.h
@@ -19,25 +19,28 @@ class EngineConfigValue {
  public:
     const static string SYSTEM_GPUID = "System/GpuId";
 
+    // feature/vehicle
     const static string FEATURE_VEHICLE_ENABLE = "Feature/Vehicle/Enable";
     const static string FEATURE_VEHICLE_ENABLE_DETECTION =
             "Feature/Vehicle/EnableDetection";
-    const static string FEATURE_VEHICLE_ENABLE_CAR_TYPE =
+    const static string FEATURE_VEHICLE_ENABLE_TYPE =
             "Feature/Vehicle/EnableType";
-    const static string FEATURE_VEHICLE_ENABLE_CAR_COLOR =
+    const static string FEATURE_VEHICLE_ENABLE_COLOR =
             "Feature/Vehicle/EnableColor";
-    const static string FEATURE_VEHICLE_ENABLE_CAR_PLATE =
+    const static string FEATURE_VEHICLE_ENABLE_PLATE =
             "Feature/Vehicle/EnablePlate";
-    const static string FEATURE_VEHICLE_ENABLE_CAR_PLATE_ENHANCED =
+    const static string FEATURE_VEHICLE_ENABLE_PLATE_ENHANCED =
             "Feature/Vehicle/EnablePlateEnhance";
-    const static string FEATURE_VEHICLE_ENABLE_CAR_MARKER =
+    const static string FEATURE_VEHICLE_ENABLE_MARKER =
             "Feature/Vehicle/EnableMarker";
     const static string FEATURE_VEHICLE_ENABLE_FEATURE_VECTOR =
             "Feature/Vehicle/EnableFeatureVector";
-
+    // feature/face
     const static string FEATURE_FACE_ENABLE = "Feature/Face/Enable";
-    const static string FEATURE_FACE_ENABLE = "Feature/Face/EnableDetection";
-    const static string FEATURE_FACE_ENABLE = "Feature/Face/EnableFeatureVector";
+    const static string FEATURE_FACE_ENABLE_DETECTION =
+            "Feature/Face/EnableDetection";
+    const static string FEATURE_FACE_ENABLE_FEATURE_VECTOR =
+            "Feature/Face/EnableFeatureVector";
 
     const static string DEBUG_MODEL_ENCRYPT = "Debug/Encrypt";
     const static string DEBUG_ENABLE = "Debug/Enable";
