5fd1bbfe2771c96d27f239bcae04b1bcd1abb9d5
zhenchen <zhenchen@deepglint.com>
2016-04-26 14:05:18 +0800

    witness engine 2nd stage

--- engine/src/processor/vehicle_multi_type_detector_processor.h
+++ engine/src/processor/vehicle_multi_type_detector_processor.h
@@ -26,68 +26,17 @@ class VehicleMultiTypeDetectorProcessor : public Processor {
  public:
 
     VehicleMultiTypeDetectorProcessor(int batch_size, bool gpu_id, int rescale,
-                                      bool is_model_encrypt)
-            : Processor() {
+                                      bool is_model_encrypt);
 
-        CaffeConfig config;
-        config.batch_size = batch_size;
-        config.is_model_encrypt = is_model_encrypt;
+    ~VehicleMultiTypeDetectorProcessor();
 
-        if (config.is_model_encrypt) {
-            config.model_file = MODEL_FILE;
-            config.deploy_file = DEPLOY_FILE;
-        } else {
-            config.model_file = MODEL_FILE_EN;
-            config.deploy_file = DEPLOY_FILE_EN;
-        }
+    void Update(Frame *frame);
 
-        config.use_gpu = true;
-        config.gpu_id = gpu_id;
-        config.rescale = rescale;
-        detector_ = new VehicleMultiTypeDetector(config);
-    }
+    void Update(FrameBatch *frameBatch);
 
-    ~VehicleMultiTypeDetectorProcessor() {
-
-    }
-
-    void Update(Frame *frame) {
-
-        DLOG(INFO)<< "Start detect frame: " << frame->id() << endl;
-        Mat data= frame->payload()->data();
-        vector<Detection> detections = detector_->Detect(data);
-
-        for (vector<Detection>::iterator itr = detections.begin();
-                itr != detections.end(); ++itr) {
-            Detection detection = *itr;
-            Object *obj;
-            if(1) {
-                Vehicle *v = new Vehicle(OBJECT_CAR);
-                obj = static_cast<Object*>(v);
-                Mat roi = Mat(data, detection.box);
-                v->set_image(roi);
-            }
-
-            obj->set_detection(detection);
-            frame->put_object(obj);
-            print(detection);
-        }
-        cout << "End detector frame: " << endl;
-        Proceed(frame);
-
-    }
-
-    void Update(FrameBatch *frameBatch) {
-
-    }
-
-    bool checkOperation(Frame *frame) {
-        return true;
-    }
-    bool checkStatus(Frame *frame) {
-        return true;
-    }
-private:
+    bool checkOperation(Frame *frame);
+    bool checkStatus(Frame *frame);
+ private:
     VehicleMultiTypeDetector *detector_;
 
 };
