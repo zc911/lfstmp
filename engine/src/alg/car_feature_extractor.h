3f8c70f126ed61417e31e61d2899e7803de03145
zhenchen <zhenchen@deepglint.com>
2016-04-27 18:52:46 +0800

    car feature extractor finish

--- engine/src/alg/car_feature_extractor.h
+++ engine/src/alg/car_feature_extractor.h
@@ -8,65 +8,24 @@
 #ifndef CAR_FEATURE_EXTRACTOR_H_
 #define CAR_FEATURE_EXTRACTOR_H_
 
-#include <vector>
 #include <opencv2/core/core.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 
 #include "model/rank_feature.h"
-
-using namespace std;
-
+#include <opencv2/features2d/features2d.hpp>
 namespace dg {
 
 class CarFeatureExtractor {
+
  public:
-    CarFeatureExtractor() {
-        orb_ = cv::ORB(256);
-        max_resize_size_ = 300;
-    }
+    CarFeatureExtractor();
+    void ExtractDescriptor(const cv::Mat &img, CarRankFeature &des);
 
-    void ExtractDescriptor(const cv::Mat &img, CarRankFeature &des) {
-
-        des.height_ = img.rows;
-        des.width_ = img.cols;
-        cv::Mat resize_img;
-        cv::Size new_size;
-        calcNewSize(des.height_, des.width_, new_size);
-
-        if (img.channels() != 3)
-            LOG(WARNING)<< "Color image is required.";
-        if ((img.rows < 10) || (img.cols < 10))
-            LOG(WARNING)<< "Image needs to be larger than 10*10 to extract enough feature.";
-
-        resize(img, resize_img, new_size);
-        vector<cv::KeyPoint> key_point;
-        cv::Mat descriptor;
-
-        orb_(resize_img, cv::Mat(), key_point, descriptor);
-        if (key_point.size() < 50)
-            LOG(WARNING)<< "Not enough feature extracted.";
-
-        descriptor.copyTo(des.descriptor_);
-        des.position_ = cv::Mat::zeros(key_point.size(), 2, CV_16UC1);
-
-        for (int i = 0; i < key_point.size(); i++) {
-            des.position_.at<ushort>(i, 0) = ((ushort) key_point[i].pt.x);
-            des.position_.at<ushort>(i, 1) = ((ushort) key_point[i].pt.y);
-        }
-
-    }
  private:
     cv::ORB orb_;
     int max_resize_size_;
 
     void calcNewSize(const ushort &ori_height, const ushort &ori_width,
-                     cv::Size &new_size) {
-        float resize_rto = max(ori_height, ori_width);
-        resize_rto = ((float) max_resize_size_) / resize_rto;
-        new_size.height = resize_rto * ori_height;
-        new_size.width = resize_rto * ori_width;
-    }
+                     cv::Size &new_size);
 };
 
 }
