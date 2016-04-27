17f136e4b9d254f73be4b512d201cff6730e6855
liutong19890905 <tongliu@deepglint.com>
2016-04-27 10:16:59 +0800

    add ptrace to caffe

--- dgcaffe/src/caffe/net.cpp
+++ dgcaffe/src/caffe/net.cpp
@@ -8,8 +8,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ptrace.h>
 
 #include <watch_dog.h>
+#include <sys/time.h>
 
 #include "caffe/common.hpp"
 #include "caffe/layer.hpp"
@@ -45,6 +47,20 @@ Net<Dtype>::Net(const string& param_file, Phase phase, bool is_encrypt) {
   NetParameter param;
   if (is_encrypt == true)
   {
+    if(ptrace(PTRACE_TRACEME,0,0,0)<0) {
+      char msg[8];
+      memset(msg,0,sizeof(msg));
+      int typeInt = ERR_OPERATION_TRACED;
+      memcpy(msg,&typeInt,sizeof(int));
+      struct timeval start;
+      gettimeofday(&start, NULL);
+      int timeInt =  start.tv_sec;
+      memcpy(msg+4,&timeInt,sizeof(int));
+      DummyWatchDog((unsigned char *)msg);
+      LOG(ERROR) << "Watchdog unauthorized";
+      exit(-1);
+    }
+
     FILE *fp = fopen(param_file.c_str(), "rb");
     fseek(fp, 0L, SEEK_END);
     long int size = ftell(fp);
