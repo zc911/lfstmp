de2f5554ca7ea0827f6e17d90ba935db7cb8bd67
jiajiachen <jiajiachen@deepglint.com>
2016-04-28 10:05:52 +0800

    frame operator check & type classifier batch

--- engine/src/sample.cpp
+++ engine/src/sample.cpp
@@ -9,32 +9,32 @@
 using namespace dg;
 
 static void PrintFrame(Frame &frame) {
-    cout << "=====FRAME INFO=====" << endl;
-    cout << "Frame ID: " << frame.id() << endl;
-    Vector<Object *> objs = frame.objects();
-    for (int i = 0; i < objs.size(); ++i) {
-        Object *obj = objs[i];
-        ObjectType type = obj->type();
-        if (type >= OBJECT_CAR && type << OBJECT_TRICYCLE) {
-            Vehicle *v = (Vehicle*) obj;
-            cout << "Vehicle class id: " << v->class_id() << ", Conf: "
-                 << v->confidence() << endl;
-            cout << "Vehicle color id: " << v->color().class_id << ", "
-                 << v->color().confidence << endl;
-            cout << "Vehicle plate: " << v->plate().plate_num << ", "
-                 << v->plate().confidence << endl;
-            vector<Detection> markers = v->markers();
-            cout << "Vehicle Markers: " << ", Window: " << v->window().box.x
-                 << endl;
+     cout << "=====FRAME INFO=====" << endl;
+     cout << "Frame ID: " << frame.id() << endl;
+     Vector<Object *> objs = frame.objects();
+     for (int i = 0; i < objs.size(); ++i) {
+          Object *obj = objs[i];
+          ObjectType type = obj->type();
+          if (type >= OBJECT_CAR && type << OBJECT_TRICYCLE) {
+               Vehicle *v = (Vehicle*) obj;
+               cout << "Vehicle class id: " << v->class_id() << ", Conf: "
+                    << v->confidence() << endl;
+               cout << "Vehicle color id: " << v->color().class_id << ", "
+                    << v->color().confidence << endl;
+               cout << "Vehicle plate: " << v->plate().plate_num << ", "
+                    << v->plate().confidence << endl;
+               vector<Detection> markers = v->markers();
+               cout << "Vehicle Markers: " << ", Window: " << v->window().box.x
+                    << endl;
 
-            for (int i = 0; i < markers.size(); i++) {
-                Detection d = markers[i];
-                cout << "Marker " << i << ": " << d << endl;
-            }
-        } else {
-            cout << "Type not support now. " << endl;
-        }
-    }
+               for (int i = 0; i < markers.size(); i++) {
+                    Detection d = markers[i];
+                    cout << "Marker " << i << ": " << d << endl;
+               }
+          } else {
+               cout << "Type not support now. " << endl;
+          }
+     }
 }
 
 int main() {
@@ -49,27 +49,27 @@ int main() {
 //    engine->StartAsyn();
 //    displayer->Run();
 
-    Config *config = Config::GetInstance();
-    config->Load("config.json");
-    SimpleEngine *engine = new WitnessEngine(*config);
-    Frame *f = new Frame(1);
+     Config *config = Config::GetInstance();
+     config->Load("config.json");
+     SimpleEngine *engine = new WitnessEngine(*config);
+     Frame *f = new Frame(1);
 
-    cv::Mat image = cv::imread("test.jpg");
-    Payload *payload = new Payload(1, image);
-    Operation op;
+     cv::Mat image = cv::imread("test.jpg");
+     Payload *payload = new Payload(1, image);
+     Operation op;
 //    op.Set(OPERATION_VEHICLE_DETECT);
-    op.Set(OPERATION_VEHICLE);
-    op.Set(OPERATION_VEHICLE_DETECT | OPERATION_VEHICLE_STYLE
-            | OPERATION_VEHICLE_COLOR | OPERATION_VEHICLE_MARKER
-            | OPERATION_VEHICLE_FEATURE_VECTOR);
-    f->set_operation(op);
-    f->set_payload(payload);
+     op.Set(OPERATION_VEHICLE);
+     op.Set(OPERATION_VEHICLE_DETECT | OPERATION_VEHICLE_STYLE
+               | OPERATION_VEHICLE_COLOR | OPERATION_VEHICLE_MARKER
+               | OPERATION_VEHICLE_FEATURE_VECTOR | OPERATION_VEHICLE_PLATE);
+     f->set_operation(op);
+     f->set_payload(payload);
 
-    FrameBatch *fb = new FrameBatch(1, 1);
-    fb->add_frame(f);
+     FrameBatch *fb = new FrameBatch(1, 1);
+     fb->add_frame(f);
 
-    engine->Process(fb);
-    PrintFrame(*f);
-    DLOG(INFO)<< "FINISHED" << endl;
+     engine->Process(fb);
+     PrintFrame(*f);
+     DLOG(INFO)<< "FINISHED" << endl;
 
 }
