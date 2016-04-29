1ca682701a921b6837ce35000f0895f3b3bd3892
zhenchen <zhenchen@deepglint.com>
2016-04-29 12:22:19 +0800

    add face rank engine

---
+++ engine/src/engine/rank_engine.h
@@ -0,0 +1,57 @@
+/*============================================================================
+ * File Name   : rank_engine.h
+ * Author      : yanlongtan@deepglint.com
+ * Version     : 1.0.0.0
+ * Copyright   : Copyright 2016 DeepGlint Inc.
+ * Created on  : 04/15/2016
+ * Description : 
+ * ==========================================================================*/
+
+#ifndef MATRIX_ENGINE_RANK_ENGINE_H_
+#define MATRIX_ENGINE_RANK_ENGINE_H_
+
+#include <glog/logging.h>
+
+#include "model/model.h"
+#include "model/rank_feature.h"
+#include "processor/processor.h"
+#include "processor/face_detect_processor.h"
+#include "processor/face_feature_extract_processor.h"
+
+
+namespace dg {
+
+class RankEngine {
+
+};
+
+//class CarRankEngine : public RankEngine {
+// public:
+//    CarRankEngine();
+//    virtual ~CarRankEngine();
+//
+//    vector<Score> Rank(const Mat& image, const Rect& hotspot,
+//                       const vector<CarRankFeature>& candidates);
+//
+// private:
+//    Identification id_;
+//    Processor *processor_;
+//};
+
+class FaceRankEngine : public RankEngine {
+
+ public:
+    FaceRankEngine();
+    virtual ~FaceRankEngine();
+    vector<Score> Rank(const Mat& image, const Rect& hotspot,
+                       const vector<FaceRankFeature>& candidates);
+
+ private:
+    Identification id_;
+    FaceDetectProcessor *detector_;
+    FaceFeatureExtractProcessor *extractor_;
+};
+
+}
+
+#endif //MATRIX_ENGINE_RANK_ENGINE_H_
