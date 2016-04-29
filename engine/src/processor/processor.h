0ecccc662e963cc64de540429f1274662de95423
zhenchen <zhenchen@deepglint.com>
2016-04-29 17:53:22 +0800

    fix bug and add Doxygen file

--- engine/src/processor/processor.h
+++ engine/src/processor/processor.h
@@ -13,12 +13,8 @@
 #include "model/frame.h"
 
 namespace dg {
-/**
- * The basic processor interface. It defines the
- * interfaces each derived processor must to implement.
- *
- *
- */
+/// The basic processor interface. It defines the
+/// interfaces each derived processor must to implement.
 class Processor {
  public:
     Processor()
@@ -40,34 +36,27 @@ class Processor {
         return next_;
     }
 
-    /**
-     * Update the input Frame.
-     */
+    /// Update the input Frame.
     virtual void Update(Frame *frame) = 0;
-    /**
-     * Update the input FrameBatch.
-     * A FrameBatch is a package of one or more Frame.
-     */
+
+    /// Update the input FrameBatch.
+    /// A FrameBatch is a package of one or more Frame.
     virtual void Update(FrameBatch *frameBatch) = 0;
 
     virtual void beforeUpdate(FrameBatch *frameBatch) = 0;
     virtual bool checkStatus(Frame *frame) = 0;
 
-    /**
-     * This method will invoke the next processor chained to the
-     * current processor.
-     * Each processor must invoke Proceed to drive the engine running.
-     */
+    /// This method will invoke the next processor chained to the
+    /// current processor.
+    /// Each processor must invoke Proceed to drive the engine running.
     virtual void Proceed(Frame *frame) {
         if (next_ != NULL) {
             next_->Update(frame);
         }
     }
-    /**
-     * This method will invoke the next processor chained to the
-     * current processor.
-     * Each processor must invoke Proceed to drive the engine running.
-     */
+    /// This method will invoke the next processor chained to the
+    /// current processor.
+    /// Each processor must invoke Proceed to drive the engine running.
     virtual void Proceed(FrameBatch *frameBatch) {
         if (next_ != NULL) {
             next_->Update(frameBatch);
