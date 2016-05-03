72af33bdd1ffe209b54ba35a1a0f870881747762
zhenchen <zhenchen@deepglint.com>
2016-05-03 12:32:02 +0800

    fix plate recognize bug. from stack to heap

--- apps/src/services/witness_service.cpp
+++ apps/src/services/witness_service.cpp
@@ -13,69 +13,86 @@
 #include "witness_service.h"
 #include "image_service.h"
 //#include "ranker_service.h"
- 
 
-namespace dg
-{
+namespace dg {
 
 WitnessAppsService::WitnessAppsService(const Config *config)
-                    : config_(config)
-                    , engine_(*config)
-                    , id_(0)
-{
+        : config_(config),
+          engine_(*config),
+          id_(0) {
 
 }
 
-WitnessAppsService::~WitnessAppsService()
-{
+WitnessAppsService::~WitnessAppsService() {
 
 }
 
-Operation WitnessAppsService::getOperation(const WitnessRequestContext& ctx)
-{
+Operation WitnessAppsService::getOperation(const WitnessRequestContext& ctx) {
     Operation op;
-    for(int i = 0; i < ctx.functions_size(); i ++)
-    {
-        switch(ctx.functions(i))
-        {
-        case RECFUNC_NONE: op.Set(OPERATION_NONE); break;
-        case RECFUNC_VEHICLE: op.Set(OPERATION_VEHICLE); break;
-        case RECFUNC_VEHICLE_DETECT: op.Set(OPERATION_VEHICLE_DETECT); break;
-        case RECFUNC_VEHICLE_TRACK: op.Set(OPERATION_VEHICLE_TRACK); break;
-        case RECFUNC_VEHICLE_STYLE: op.Set(OPERATION_VEHICLE_STYLE); break;
-        case RECFUNC_VEHICLE_COLOR: op.Set(OPERATION_VEHICLE_COLOR); break;
-        case RECFUNC_VEHICLE_MARKER: op.Set(OPERATION_VEHICLE_MARKER); break;
-        case RECFUNC_VEHICLE_PLATE: op.Set(OPERATION_VEHICLE_PLATE); break;
-        case RECFUNC_VEHICLE_FEATURE_VECTOR: op.Set(OPERATION_VEHICLE_FEATURE_VECTOR); break;
-        case RECFUNC_FACE: op.Set(OPERATION_FACE); break;
-        case RECFUNC_FACE_DETECTOR: op.Set(OPERATION_FACE_DETECTOR); break;
-        case RECFUNC_FACE_FEATURE_VECTOR: op.Set(OPERATION_FACE_FEATURE_VECTOR); break;
-        default: break;
+    for (int i = 0; i < ctx.functions_size(); i++) {
+        switch (ctx.functions(i)) {
+            case RECFUNC_NONE:
+                op.Set(OPERATION_NONE);
+                break;
+            case RECFUNC_VEHICLE:
+                op.Set(OPERATION_VEHICLE);
+                break;
+            case RECFUNC_VEHICLE_DETECT:
+                op.Set(OPERATION_VEHICLE_DETECT);
+                break;
+            case RECFUNC_VEHICLE_TRACK:
+                op.Set(OPERATION_VEHICLE_TRACK);
+                break;
+            case RECFUNC_VEHICLE_STYLE:
+                op.Set(OPERATION_VEHICLE_STYLE);
+                break;
+            case RECFUNC_VEHICLE_COLOR:
+                op.Set(OPERATION_VEHICLE_COLOR);
+                break;
+            case RECFUNC_VEHICLE_MARKER:
+                op.Set(OPERATION_VEHICLE_MARKER);
+                break;
+            case RECFUNC_VEHICLE_PLATE:
+                op.Set(OPERATION_VEHICLE_PLATE);
+                break;
+            case RECFUNC_VEHICLE_FEATURE_VECTOR:
+                op.Set(OPERATION_VEHICLE_FEATURE_VECTOR);
+                break;
+            case RECFUNC_FACE:
+                op.Set(OPERATION_FACE);
+                break;
+            case RECFUNC_FACE_DETECTOR:
+                op.Set(OPERATION_FACE_DETECTOR);
+                break;
+            case RECFUNC_FACE_FEATURE_VECTOR:
+                op.Set(OPERATION_FACE_FEATURE_VECTOR);
+                break;
+            default:
+                break;
         }
     }
 
     return op;
 }
 
-void WitnessAppsService::copyCutboard(const Box &b, Cutboard *cb)
-{
+void WitnessAppsService::copyCutboard(const Box &b, Cutboard *cb) {
     cb->set_x(b.x);
     cb->set_y(b.y);
     cb->set_width(b.width);
     cb->set_height(b.height);
 }
 
-MatrixError WitnessAppsService::fillModel(Identification id, VehicleModel *model)
-{
+MatrixError WitnessAppsService::fillModel(Identification id,
+                                          VehicleModel *model) {
     MatrixError err;
 
-    model->set_brandid(id);//just set class id to brandid for test
+    model->set_brandid(id);  //just set class id to brandid for test
 
     return err;
 }
 
-MatrixError WitnessAppsService::fillColor(const Vehicle::Color &color, model::Color *rcolor)
-{
+MatrixError WitnessAppsService::fillColor(const Vehicle::Color &color,
+                                          model::Color *rcolor) {
     MatrixError err;
 
     rcolor->set_id(color.class_id);
@@ -84,8 +101,8 @@ MatrixError WitnessAppsService::fillColor(const Vehicle::Color &color, model::Co
     return err;
 }
 
-MatrixError WitnessAppsService::fillPlate(const Vehicle::Plate &plate, LicensePlate *rplate)
-{
+MatrixError WitnessAppsService::fillPlate(const Vehicle::Plate &plate,
+                                          LicensePlate *rplate) {
     MatrixError err;
     rplate->set_platenum(plate.plate_num);
     copyCutboard(plate.box, rplate->mutable_cutboard());
@@ -96,16 +113,16 @@ MatrixError WitnessAppsService::fillPlate(const Vehicle::Plate &plate, LicensePl
     return err;
 }
 
-MatrixError WitnessAppsService::fillSymbols(const vector<Object*>& objects, RecognizedVehicle *vrec)
-{
+MatrixError WitnessAppsService::fillSymbols(const vector<Object*>& objects,
+                                            RecognizedVehicle *vrec) {
     MatrixError err;
 
     int isize = 7;  //TODO: change 6 to real size
     int* indexes = new int[isize];
-    for(int i = 0; i < isize; i ++)indexes[i] = -1;
-    for(const Object *object : objects)
-    {
-        LOG(INFO) << "recognized object(marker?): " << object->id() << ", type: " << object->type();
+    for (int i = 0; i < isize; i++)
+        indexes[i] = -1;
+    for (const Object *object : objects) {
+        LOG(INFO)<< "recognized object(marker?): " << object->id() << ", type: " << object->type();
         if (object->type() != OBJECT_MARKER)
         {
             LOG(WARNING) << "unknown marker type: " << object->type();
@@ -128,7 +145,7 @@ MatrixError WitnessAppsService::fillSymbols(const vector<Object*>& objects, Reco
                 item = vrec->mutable_symbolitems( indexes[mid] );
             }
 
-            Symbol *s =  item->add_symbols();
+            Symbol *s = item->add_symbols();
             s->set_confidence(m->detection().confidence);
             copyCutboard(m->detection().box, s->mutable_cutboard());
         }
@@ -138,60 +155,63 @@ MatrixError WitnessAppsService::fillSymbols(const vector<Object*>& objects, Reco
     return err;
 }
 
-MatrixError WitnessAppsService::getRecognizedVehicle(Vehicle *vobj, RecognizedVehicle *vrec)
-{
+MatrixError WitnessAppsService::getRecognizedVehicle(Vehicle *vobj,
+                                                     RecognizedVehicle *vrec) {
     MatrixError err;
     vrec->set_features(vobj->feature().Serialize());
 
-    const Detection &d = vobj->window();
-    LOG(INFO) << "detection id: " << d.id << ", deleted? " << d.deleted;
+    const Detection &d = vobj->detection();
+    LOG(INFO)<< "detection id: " << d.id << ", deleted? " << d.deleted;
     copyCutboard(d.box, vrec->mutable_cutboard());
 
     err = fillModel(vobj->class_id(), vrec->mutable_model());
-    if (err.code() < 0)return err;
+    if (err.code() < 0)
+        return err;
 
     err = fillColor(vobj->color(), vrec->mutable_color());
-    if (err.code() < 0)return err;
+    if (err.code() < 0)
+        return err;
 
     err = fillPlate(vobj->plate(), vrec->mutable_licenseplate());
-    if (err.code() < 0)return err;
+    if (err.code() < 0)
+        return err;
 
     err = fillSymbols(vobj->children(), vrec);
-    if (err.code() < 0)return err;
+    if (err.code() < 0)
+        return err;
 
     return err;
 }
 
-MatrixError WitnessAppsService::getRecognizedFace(Face *fobj, RecognizedFace *frec)
-{
+MatrixError WitnessAppsService::getRecognizedFace(Face *fobj,
+                                                  RecognizedFace *frec) {
     MatrixError err;
-    frec->set_confidence((float)fobj->confidence());
+    frec->set_confidence((float) fobj->confidence());
     frec->set_features(fobj->feature().Serialize());
 
     const Detection &d = fobj->detection();
-    LOG(INFO) << "detection id: " << d.id << ", deleted? " << d.deleted;
+    LOG(INFO)<< "detection id: " << d.id << ", deleted? " << d.deleted;
     copyCutboard(d.box, frec->mutable_cutboard());
     return err;
 }
 
-MatrixError WitnessAppsService::getRecognizeResult(Frame *frame, WitnessResult *result)
-{
+MatrixError WitnessAppsService::getRecognizeResult(Frame *frame,
+                                                   WitnessResult *result) {
     MatrixError err;
 
-    for(const Object *object : frame->objects())
-    {
-        LOG(INFO) << "recognized object: " << object->id() << ", type: " << object->type();
+    for (const Object *object : frame->objects()) {
+        LOG(INFO)<< "recognized object: " << object->id() << ", type: " << object->type();
         switch(object->type())
         {
-        case OBJECT_CAR:
+            case OBJECT_CAR:
             err = getRecognizedVehicle((Vehicle *)object, result->add_vehicles());
             break;
 
-        case OBJECT_FACE:
+            case OBJECT_FACE:
             err = getRecognizedFace((Face *)object, result->add_faces());
             break;
 
-        default:
+            default:
             LOG(WARNING) << "unknown object type: " << object->type();
         }
 
@@ -204,32 +224,30 @@ MatrixError WitnessAppsService::getRecognizeResult(Frame *frame, WitnessResult *
     return err;
 }
 
-bool WitnessAppsService::Recognize(const WitnessRequest *request, WitnessResponse *response)
-{
+bool WitnessAppsService::Recognize(const WitnessRequest *request,
+                                   WitnessResponse *response) {
     struct timeval curr_time;
     gettimeofday(&curr_time, NULL);
-    
+
     const string& sessionid = request->context().sessionid();
 
-    if (!request->has_image() || !request->image().has_data())
-    {
-        LOG(ERROR) << "image descriptor does not exist";
+    if (!request->has_image() || !request->image().has_data()) {
+        LOG(ERROR)<< "image descriptor does not exist";
         return false;
     }
 
-    LOG(INFO) << "Get Recognize request: " << sessionid
-              << ", Image URI:" << request->image().data().uri();
-    LOG(INFO) << "Start processing: " << sessionid << "...";
+    LOG(INFO)<< "Get Recognize request: " << sessionid
+    << ", Image URI:" << request->image().data().uri();
+    LOG(INFO)<< "Start processing: " << sessionid << "...";
 
     Mat image;
     MatrixError err = ImageService::ParseImage(request->image().data(), image);
-    if (err.code() != 0)
-    {
-        LOG(ERROR) << "parse image failed, " << err.message();
+    if (err.code() != 0) {
+        LOG(ERROR)<< "parse image failed, " << err.message();
         return false;
     }
 
-    Identification curr_id = id_ ++; //TODO: make thread safe
+    Identification curr_id = id_++;  //TODO: make thread safe
     Frame *frame = new Frame(curr_id, image);
     frame->set_operation(getOperation(request->context()));
 
@@ -240,21 +258,20 @@ bool WitnessAppsService::Recognize(const WitnessRequest *request, WitnessRespons
     //fill response
     ::dg::WitnessResponseContext* ctx = response->mutable_context();
     ctx->set_sessionid(sessionid);
-    ctx->mutable_requestts()->set_seconds((int64_t)curr_time.tv_sec);
-    ctx->mutable_requestts()->set_nanosecs((int64_t)curr_time.tv_usec);
+    ctx->mutable_requestts()->set_seconds((int64_t) curr_time.tv_sec);
+    ctx->mutable_requestts()->set_nanosecs((int64_t) curr_time.tv_usec);
     ctx->set_status("200");
     ctx->set_message("SUCCESS");
 
     //debug information of this request
-    ::google::protobuf::Map<::std::string, ::dg::Time>& debugTs = *ctx->mutable_debugts();
+    ::google::protobuf::Map<::std::string, ::dg::Time>& debugTs = *ctx
+            ->mutable_debugts();
     debugTs["start"] = ctx->requestts();
-
 
     WitnessResult *result = response->mutable_result();
     err = getRecognizeResult(frame, result);
-    if (err.code() != 0)
-    {
-        LOG(ERROR) << "get result from frame failed, " << err.message();
+    if (err.code() != 0) {
+        LOG(ERROR)<< "get result from frame failed, " << err.message();
         return false;
     }
 
@@ -265,8 +282,8 @@ bool WitnessAppsService::Recognize(const WitnessRequest *request, WitnessRespons
     //if ReturnsImage, compress image into data.bindata
 
     gettimeofday(&curr_time, NULL);
-    ctx->mutable_responsets()->set_seconds((int64_t)curr_time.tv_sec);
-    ctx->mutable_responsets()->set_nanosecs((int64_t)curr_time.tv_usec);
+    ctx->mutable_responsets()->set_seconds((int64_t) curr_time.tv_sec);
+    ctx->mutable_responsets()->set_nanosecs((int64_t) curr_time.tv_usec);
     debugTs["end"] = ctx->responsets();
 
     cout << "recognized objects: " << frame->objects().size() << endl;
@@ -276,8 +293,8 @@ bool WitnessAppsService::Recognize(const WitnessRequest *request, WitnessRespons
     return true;
 }
 
-bool WitnessAppsService::BatchRecognize(const WitnessBatchRequest *request, WitnessBatchResponse *response)
-{
+bool WitnessAppsService::BatchRecognize(const WitnessBatchRequest *request,
+                                        WitnessBatchResponse *response) {
 
     return true;
 }
