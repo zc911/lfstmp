a10f7b901f13b801329ded03184175f0ff6cb378
jiajiachen <jiajiachen@deepglint.com>
2016-05-05 20:13:56 +0800

    before create engine_helper

--- engine/src/engine/witness_engine.h
+++ engine/src/engine/witness_engine.h
@@ -9,6 +9,7 @@
 #define WITNESS_ENGINE_H_
 
 #include "config.h"
+#include "fs_util.h"
 #include "simple_engine.h"
 #include "processor/processor.h"
 #include "engine_config_value.h"
@@ -33,24 +34,31 @@ class WitnessEngine : public SimpleEngine {
  private:
     void init(const Config &config);
     void initFeatureOptions(const Config &config);
-
+    int initDataConfig(const Config &config, Config &data_config);
     void createVehicleConfig(
-            const Config &cconfig,
+            const Config &cconfig, const Config &data_config,
             vector<VehicleCaffeClassifier::VehicleCaffeConfig> & configs);
 
     void createVehicleColorConfig(
-            const Config &config,vector<VehicleCaffeClassifier::VehicleCaffeConfig> &);
-    void createMarkersConfig(
-            const Config &config,MarkerCaffeClassifier::MarkerConfig &);
+            const Config &config, const Config &dataconfig,
+            vector<VehicleCaffeClassifier::VehicleCaffeConfig> &);
+    void createMarkersConfig(const Config &config, const Config &data_config,
+                             MarkerCaffeClassifier::MarkerConfig &);
     void createVehicleMutiTypeDetectorConfig(
-            const Config &cconfig,
+            const Config &cconfig, const Config &data_config,
             VehicleMultiTypeDetector::VehicleMultiTypeConfig & config);
-    void createWindowConfig(
-            const Config &config,WindowCaffeDetector::WindowCaffeConfig &);
+    void createWindowConfig(const Config &config, const Config &data_config,
+                            WindowCaffeDetector::WindowCaffeConfig &);
 
-    void createVehiclePlateConfig(
-            const Config &config,PlateRecognizer::PlateConfig &);
-
+    void createVehiclePlateConfig(const Config &config,
+                                  const Config &data_config,
+                                  PlateRecognizer::PlateConfig &);
+    void createFaceDetectorConfig(const Config &cconfig,
+                                  const Config &data_config,
+                                  FaceDetector::FaceDetectorConfig & config);
+    void createFaceExtractorConfig(const Config &cconfig,
+                                  const Config &data_config,
+                                  FaceFeatureExtractor::FaceFeatureExtractorConfig & config);
     Processor *vehicle_processor_;
     Processor *face_processor_;
 
