721c384b5045fe81ab5d1422d7b3a3a442fdc8d0
zhenchen <zhenchen@deepglint.com>
2016-05-19 14:42:11 +0800

    a new witness press testing python script

--- apps/src/main.cpp
+++ apps/src/main.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 using namespace dg;
 
-void serveGrpc(const Config* config) {
+void serveGrpc(const Config *config) {
     string instType = (string) config->Value("InstanceType");
     cout << "Instance type: " << instType << endl;
 
@@ -33,7 +33,7 @@ void serveGrpc(const Config* config) {
     }
 
     string address = (string) config->Value("System/Ip") + ":"
-            + (string) config->Value("System/Port");
+        + (string) config->Value("System/Port");
 
     grpc::ServerBuilder builder;
     builder.AddListeningPort(address, grpc::InsecureServerCredentials());
@@ -43,7 +43,7 @@ void serveGrpc(const Config* config) {
     server->Wait();
 }
 
-void serveHttp(const Config* config) {
+void serveHttp(const Config *config, int userPort = 0) {
     string instType = (string) config->Value("InstanceType");
     cout << "Instance type: " << instType << endl;
 
@@ -58,6 +58,10 @@ void serveHttp(const Config* config) {
     }
 
     int port = (int) config->Value("System/Port");
+    if (userPort) {
+        port = userPort;
+    }
+
     SimpleWeb::Server<SimpleWeb::HTTP> server(port, 1);  //at port with 1 thread
     service->Bind(server);
 
@@ -65,19 +69,29 @@ void serveHttp(const Config* config) {
     server.start();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+
     google::InitGoogleLogging(argv[0]);
+
     google::ParseCommandLineFlags(&argc, &argv, true);
 
+    int userPort = 0;
+    if (argc == 2) {
+        userPort = atoi(argv[1]);
+    }
+
     Config *config = new Config();
+
+
     config->Load("config.json");
+
 
     string protocolType = (string) config->Value("ProtocolType");
     cout << "Protocol type: " << protocolType << endl;
     if (protocolType == "rpc") {
         serveGrpc(config);
     } else if (protocolType == "restful") {
-        serveHttp(config);
+        serveHttp(config, userPort);
     } else {
         cout << "unknown protocol type: " << protocolType << endl;
     }
