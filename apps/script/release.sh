51bb65774686c9a78ba107b940723e79649196d7
zhenchen <zhenchen@deepglint.com>
2016-05-23 12:58:22 +0800

    update release and install script

---
+++ apps/script/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+if [ -z "$1" ]; then
+	echo "Please input version number!"
+	exit
+fi
+
+version=$1
+name="matrix_apps"
+platform=$(uname -s)-$(uname -p)
+rm ../bin/Release/*.md
+cp ../README.md ../bin/Release/
+cp ../release/release_note_$version.md ../bin/Release/
+tar -cvf $name"_"$version.tar ../bin/Release/*
+rm version && touch version
+echo $version > version
+scp $name"_"$version.tar  dell@192.168.2.119:~/release/home/matrix/$platform/
+scp version dell@192.168.2.119:~/release/home/matrix/$platform/
+rm version
+rm $name"_"$1.tar
+
+echo "release " $version "done!"
+
