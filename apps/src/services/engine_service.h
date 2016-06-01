f8dcd9b7a9f8a79ad6454bcd2eb0116fa448f116
zhenchen <zhenchen@deepglint.com>
2016-06-01 20:26:04 +0800

    restful works perfectly

---
+++ apps/src/services/engine_service.h
@@ -0,0 +1,25 @@
+//
+// Created by chenzhen on 6/1/16.
+//
+
+#ifndef PROJECT_ENGINE_SERVICE_H
+#define PROJECT_ENGINE_SERVICE_H
+#include "matrix_engine/model/model.h"
+#include "model/witness.grpc.pb.h"
+#include "engine_service.h"
+
+using namespace ::dg::model;
+namespace dg {
+
+class EngineService {
+public:
+    virtual MatrixError Recognize(const WitnessRequest *request, WitnessResponse *response){}
+
+    virtual MatrixError BatchRecognize(const WitnessBatchRequest *request,
+                               WitnessBatchResponse *response){}
+    virtual ~EngineService() { }
+};
+
+}
+
+#endif //PROJECT_ENGINE_SERVICE_H
