36c7d360eb5df71d5ced0e05e2906ba44291a1d1
jiajiachen <jiajiachen@deepglint.com>
2016-06-04 16:24:27 +0800

    system service except network done

--- apps/src/restful/restful.h
+++ apps/src/restful/restful.h
@@ -17,70 +17,98 @@
 using namespace std;
 using namespace ::dg::model;
 namespace dg {
-
+enum ErrorCode {
+    NoError = 200,
+    ServiceError = 500,
+    RequestError = 400
+};
 template<class request_type, class response_type>
 using BindFunction = std::function<MatrixError(const request_type *, response_type *)>;
 
-typedef SimpleWeb::Server <SimpleWeb::HTTP> HttpServer;
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 
 class RestfulService {
-public:
+ public:
     RestfulService(string protocol = "HTTP/1.1", string mime_type =
-    "application/json; charset=utf-8")
-        : protocol_(protocol),
-          mime_type_(mime_type) {
+                           "application/json; charset=utf-8")
+            : protocol_(protocol),
+              mime_type_(mime_type) {
     }
     virtual ~RestfulService() {
     }
 
     virtual void Bind(HttpServer &server) = 0;
 
-protected:
+ protected:
     template<class request_type, class response_type>
     static void bind(
-        HttpServer &server, string endpoint, string method,
-        std::function<MatrixError(const request_type *, response_type *)> func) {
-        server.resource[endpoint][method] =
-            [func](HttpServer::Response &response, std::shared_ptr<HttpServer::Request> request) {
-              request_type protobufRequestMessage;
-              response_type protobufResponseMessage;
+            HttpServer &server,
+            string endpoint,
+            string method,
+            std::function<MatrixError(const request_type *, response_type *)> func) {
+        if (method == ("GET")) {
+            server.resource[endpoint][method] =
+                    [func](HttpServer::Response &response, std::shared_ptr<HttpServer::Request> request) {
+                        request_type protobufRequestMessage;
+                        response_type protobufResponseMessage;
+                        try {
+                            MatrixError error = func(&protobufRequestMessage, &protobufResponseMessage);
+                            if (error.code() != 0) {
+                                responseText(response, ServiceError, error.message());
+                                return;
+                            }
+                            string content = "";
+                            pbjson::pb2json(&protobufResponseMessage, content);
+                            responseText(response, NoError, content);
+                        }
+                        catch (exception &e) {
+                            responseText(response, ServiceError, e.what());
+                        }
+                    };
+        } else if (method == "POST") {
+            server.resource[endpoint][method] =
+                    [func](HttpServer::Response &response, std::shared_ptr<HttpServer::Request> request) {
+                        request_type protobufRequestMessage;
+                        response_type protobufResponseMessage;
+                        try {
+                            string content = request->content.string();
+                            string err;
+                            int ret = pbjson::json2pb(content, &protobufRequestMessage, err);
+                            if (ret < 0) {
+                                responseText(response, RequestError, "parameter conversion failed: " + err);
+                                return;
+                            }
+                            MatrixError error = func(&protobufRequestMessage, &protobufResponseMessage);
+                            if (error.code() != 0) {
+                                responseText(response, ServiceError, error.message());
+                                return;
+                            }
 
-              try {
-                  string content = request->content.string();
-                  string err;
-                  int ret = pbjson::json2pb(content, &protobufRequestMessage, err);
-                  if (ret < 0) {
-                      responseText(response, 400, "parameter conversion failed: " + err);
-                      return;
-                  }
-                  MatrixError error = func(&protobufRequestMessage, &protobufResponseMessage);
-                  if (error.code() != 0) {
-                      responseText(response, 500, error.message());
-                      return;
-                  }
+                            content = "";
+                            pbjson::pb2json(&protobufResponseMessage, content);
+                            responseText(response, NoError, content);
+                        }
+                        catch (exception &e) {
+                            responseText(response, ServiceError, e.what());
+                        }
+                    };
+        }
 
-                  content = "";
-                  pbjson::pb2json(&protobufResponseMessage, content);
-                  responseText(response, 200, content);
-              }
-              catch (exception &e) {
-                  responseText(response, 500, e.what());
-              }
-            };
     }
 
-private:
+ private:
     string protocol_;
     string mime_type_;
 
     static void responseText(HttpServer::Response &response, int code,
                              const string &text) {
         response << "HTTP/1.1 " << std::to_string(code)
-            << "\r\nContent-Length: " << text.length()
-            << "\r\nContent-Type: application/json; charset=utf-8\r\n\r\n"
-            << text;
+                << "\r\nContent-Length: " << text.length()
+                << "\r\nContent-Type: application/json; charset=utf-8\r\n\r\n"
+                << text;
     }
-};
+}
+;
 
 }
 
