6faef9308bb146848db10b5d4003a7b4854362c4
jiajiachen <jiajiachen@deepglint.com>
2016-06-09 11:14:15 +0800

    without assembling results

--- apps/src/grpc/spring_grpc.h
+++ apps/src/grpc/spring_grpc.h
@@ -13,7 +13,9 @@ using namespace ::dg::model;
 using ::dg::model::SpringService;
 using grpc::Channel;
 using grpc::ClientContext;
+using grpc::ClientAsyncResponseReader;
 using grpc::Status;
+using grpc::CompletionQueue;
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
@@ -24,15 +26,31 @@ public:
     }
     string Index(GenericObj &request,
                       NullMessage *reply){
-        ClientContext context;
-        Status status = stub_->Index(&context,request,reply);
-        if (status.ok()) {
-            return "reply successed";
-        } else {
-            return "reply failed";
+        AsyncClientCall *call = new AsyncClientCall;
+        call->response_reader=stub_->AsyncIndex(&call->context,request,&cq_);
+        call->response_reader->Finish(&call->reply,&call->status,(void *)call);
+    }
+    void AsyncCompleteRpc(){
+        void *got_tag;
+        bool ok=false;
+        while(cq_.Next(&got_tag,&ok)){
+            AsyncClientCall *call = static_cast<AsyncClientCall*>(got_tag);
+            GPR_ASSERT(ok);
+            if(call->status.ok())
+                std::cout<<"receivced"<<std::endl;
+            else
+                std::cout<<"rpc failed"<<std::endl;
+            delete call;
         }
     }
 private:
+    struct AsyncClientCall{
+        NullMessage reply;
+        ClientContext context;
+        Status status;
+        std::unique_ptr<ClientAsyncResponseReader<NullMessage> > response_reader;
+    };
     std::unique_ptr<SpringService::Stub> stub_;
+    CompletionQueue cq_;
 };
 #endif //MATRIX_APPS_SPRING_GRPC_H
