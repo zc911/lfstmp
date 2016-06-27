8451ae67fcee7a3ac03f17be186caaae10ce7953
jiajiachen <jiajiachen@deepglint.com>
2016-06-27 10:39:38 +0800

    create class

---
+++ engine/src/alg/caffe_vehicle_color_classifier.h
@@ -0,0 +1,14 @@
+//
+// Created by jiajaichen on 16-6-27.
+//
+
+#ifndef PROJECT_CAFFE_VEHICLE_COLOR_CLASSIFIER_H
+#define PROJECT_CAFFE_VEHICLE_COLOR_CLASSIFIER_H
+
+
+class CaffeVehicleColorClassifier {
+
+};
+
+
+#endif //PROJECT_CAFFE_VEHICLE_COLOR_CLASSIFIER_H
