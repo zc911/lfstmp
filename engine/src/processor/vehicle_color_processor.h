70ed08f8fc36c11473a92266ec30b38e7231f2f6
jiajiachen <jiajiachen@deepglint.com>
2016-06-27 20:47:37 +0800

    merge color done

--- engine/src/processor/vehicle_color_processor.h
+++ engine/src/processor/vehicle_color_processor.h
@@ -12,13 +12,13 @@
 #include "alg/vehicle_caffe_classifier.h"
 #include "processor_helper.h"
 #include "alg/caffe_vehicle_color_classifier.h"
-
+#include "model/basic.h"
 namespace dg {
-
+static int SHIFT_COLOR=1000;
 class VehicleColorProcessor : public Processor {
  public:
     VehicleColorProcessor(
-            const vector<VehicleCaffeClassifier::VehicleCaffeConfig> &configs);
+            const vector<CaffeVehicleColorClassifier::VehicleColorConfig> &configs);
     ~VehicleColorProcessor();
 
  protected:
@@ -31,15 +31,43 @@ class VehicleColorProcessor : public Processor {
 
 
     virtual bool RecordFeaturePerformance();
+    void score_color(Prediction &max,vector<Prediction> preds){
+        Prediction min = nthPrediction(preds,0);
+        Prediction fth = nthPrediction(preds,preds.size()-1);
+        Prediction sth = nthPrediction(preds,preds.size()-2);
+        Prediction tth = nthPrediction(preds,preds.size()-3);
+        float tot_score = 0;
+        for (int i=0; i<preds.size(); i++) {
+            tot_score += preds[i].second;
+        }
+        float high_thr=color_high_thr_*tot_score+min.second;
+        float low_thr=color_low_thr_*tot_score+min.second;
+        if(fth.second>high_thr){
+            max=fth;
+            return;
+        }
+        if(tth.second>low_thr){
+            max.first=-1;
+            max.second=0;
+            return;
+        }
+        if(sth.second>low_thr){
+            int tmp_min=std::min(fth.first,sth.first);
+            int tmp_max=std::max(fth.first,sth.first);
 
+            max.first=preds.size()*(tmp_min+1)-(tmp_min)*(tmp_min+1)/2+tmp_max-tmp_min-1;
+            max.second=1;
+        }
+    }
  protected:
     void vehiclesResizedMat(FrameBatch *frameBatch);
  private:
 
-    vector<VehicleCaffeClassifier*> classifiers_;
-    CaffeVehicleColorClassifier * classifier_;
+    vector<CaffeVehicleColorClassifier*> classifiers_;
     vector<Object *> objs_;
     vector<Mat> images_;
+    float color_low_thr_ = 0.3;
+    float color_high_thr_ = 0.5;
 
 };
 
