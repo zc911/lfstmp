064e301bf3600b0379add568b883cdfcee3b1007
jiajiachen <jiajiachen@deepglint.com>
2016-06-29 11:01:46 +0800

    new color & accelerate

--- apps/src/grpc/spring_grpc.h
+++ apps/src/grpc/spring_grpc.h
@@ -15,30 +15,17 @@
 using namespace std;
 using namespace ::dg::model;
 namespace dg {
-class SpringGrpcClientImpl final: public BasicGrpcClient < StorageRequest > {
+class SpringGrpcClientImpl{
 public:
-    SpringGrpcClientImpl(Config config,
-                         MessagePool<StorageRequest> *storage_pool)
-        : BasicGrpcClient<StorageRequest>(config,storage_pool), config_(&config) {
+    SpringGrpcClientImpl(Config config)
+        :config_(&config) {
 
     }
-
-
 
     virtual void Run() {
-        message_pool_->Run();
-        test();
-    }
-    void test(){
+        StorageRequest sr(config_);
         while(1){
-        CallData data;
-        data.func = [&data]() -> MatrixError {
-          return (bind(&StorageRequest::storage,
-                       ( StorageRequest *) data.apps)());
-        };
-
-        message_pool_->enqueue(&data);
-        MatrixError error = data.Wait();
+            sr.storage();
         }
     }
 
