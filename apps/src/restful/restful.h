f905f6def55a031fe0229e9e1cc0b29f6b7a1051
jiajiachen <jiajiachen@deepglint.com>
2016-06-30 15:40:02 +0800

    http protocol

--- apps/src/restful/restful.h
+++ apps/src/restful/restful.h
@@ -36,7 +36,7 @@ using BindFunction = std::function<MatrixError(const request_type *, response_ty
 typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 static void responseText(HttpServer::Response &response, int code,
                          const string &text) {
-    response << "HTTP/2.1 " << std::to_string(code)
+    response << "HTTP/1.1 " << std::to_string(code)
         << "\r\nContent-Length: " << text.length()
         << "\r\nContent-Type: application/json; charset=utf-8\r\n\r\n"
         << text;
