2bf1ea53def2b15020c177c617f2be11113c7145
zhenchen <zhenchen@deepglint.com>
2016-07-10 18:50:28 +0800

    car only run now

--- engine/src/processor/vehicle_multi_type_detector_processor.h
+++ engine/src/processor/vehicle_multi_type_detector_processor.h
@@ -10,10 +10,11 @@
 
 #include <vector>
 #include <glog/logging.h>
-#include <alg/detector/car_only_caffe_detector.h>
+#include "util/debug_util.h"
 #include "processor.h"
 #include "alg/detector/vehicle_caffe_detector.h"
-#include "util/debug_util.h"
+#include "alg/detector/car_only_confirm_caffe_detector.h"
+#include "alg/detector/car_only_caffe_detector.h"
 
 using namespace std;
 namespace dg {
@@ -41,6 +42,7 @@ protected:
 private:
     VehicleCaffeDetector *vehicle_detector_;
     CarOnlyCaffeDetector *car_only_detector_;
+    CarOnlyConfirmCaffeDetector *car_only_confirm_;
     VehicleCaffeDetectorConfig config_;
     int base_id_;
 
