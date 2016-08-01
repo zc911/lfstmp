a20cc7308bc881a17dd20a070ac1880f722dda3b
Sun Xiaodong <xiaodongsun@deepglint.com>
2016-08-01 21:10:58 +0800

    update cmakelists to support gtest

--- apps/test/test_main.cpp
+++ apps/test/test_main.cpp
@@ -9,9 +9,3 @@ TEST(FirstTest, test1) {
     ImageService imageService;
     EXPECT_TRUE(true);
 }
-
-int main(int argc, char* argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
