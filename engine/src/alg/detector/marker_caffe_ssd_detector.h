97499298037709147ce0bdd1c082677ce5409145
jiajiachen <jiajiachen@deepglint.com>
2016-08-05 14:38:53 +0800

    aa

---
+++ engine/src/alg/detector/marker_caffe_ssd_detector.h
@@ -0,0 +1,58 @@
+//
+// Created by jiajaichen on 16-8-5.
+//
+
+#ifndef SRC_ALG_DETECTOR_MARKER_CAFFE_SSD_DETECTOR_H
+#define SRC_ALG_DETECTOR_MARKER_CAFFE_SSD_DETECTOR_H
+#include <cassert>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "dgcaffe/caffe/caffe.hpp"
+#include "model/basic.h"
+#include "model/model.h"
+#include "alg/detector/detector.h"
+#include "alg/caffe_config.h"
+
+using namespace std;
+using namespace cv;
+using namespace caffe;
+
+namespace dg {
+
+class MarkerCaffeSsdDetector: public MarkerCaffeSsdDetector {
+
+public:
+
+    MarkerCaffeSsdDetector(const VehicleCaffeDetectorConfig &config);
+    virtual ~MarkerCaffeSsdDetector();
+    virtual int DetectBatch(vector<cv::Mat> &img,vector<Detection> &window_detections,
+                            vector<vector<Detection> > &detect_results);
+
+protected:
+
+    vector<Blob<float>*> PredictBatch(const vector<Mat> &imgs);
+    void Fullfil(vector<cv::Mat> &img, vector<Blob<float> *> &outputs,vector<vector<Detection> > &detect_results,vector<vector<Rect> > fobs,vector<vector<Rect> >&params);
+
+private:
+    boost::shared_ptr<caffe::Net<float>> net_;
+    int num_channels_;
+    cv::Size input_geometry_;
+    bool device_setted_;
+    VehicleCaffeDetectorConfig caffe_config_;
+    bool use_gpu_;
+    int gpu_id_;
+    int batch_size_;
+    float threshold_;
+#ifdef SHOW_VIS
+    vector<Scalar> color_;
+    vector<string> tags_;
+#endif
+
+};
+}
+
+
+
+#endif //PROJECT_MARKER_CAFFE_SSD_DETECTOR_H
