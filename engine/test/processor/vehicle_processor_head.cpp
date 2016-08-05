df062b32fca163e59c2788b1e0ddcc389789969c
Sun Xiaodong <xiaodongsun@deepglint.com>
2016-08-05 16:34:19 +0800

    add encrypted models and unencrypted models unit_test

--- engine/test/processor/vehicle_processor_head.cpp
+++ engine/test/processor/vehicle_processor_head.cpp
@@ -19,12 +19,21 @@ void VehicleProcessorHead::init() {
 
 VehicleCaffeDetectorConfig VehicleProcessorHead::getConfig() {
     VehicleCaffeDetectorConfig config;
+    string basePath;
+
+#ifdef UNENCRYPTMODEL
     config.is_model_encrypt = false;
+    basePath = "data/0/";
+#else
+    config.is_model_encrypt = true;
+    basePath = "data/1/";
+#endif
+
     config.target_max_size = 600;
     config.target_min_size = 400;
     config.deploy_file = config.confirm_deploy_file =
-            "data/models/300.txt";
+            basePath + "300.txt";
     config.model_file = config.confirm_deploy_file =
-            "data/models/300.dat";
+            basePath + "300.dat";
     return config;
 }
