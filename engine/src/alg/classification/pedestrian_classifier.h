e31bbc0df163263b3443c87e0fe444cb1db28ff6
Sun Xiaodong <xiaodongsun@deepglint.com>
2016-08-10 09:52:12 +0800

    change table into four blanks

--- engine/src/alg/classification/pedestrian_classifier.h
+++ engine/src/alg/classification/pedestrian_classifier.h
@@ -39,16 +39,16 @@ public:
     typedef struct {
         int index;
         string tagname;
-		float threshold;
-		float threshold_t;
+        float threshold;
+        float threshold_t;
     } Tag;
 
     typedef struct {
         int index;
         string tagname;
         float confidence;
-		float threshold;
-		float threshold_t;
+        float threshold;
+        float threshold_t;
     } PedestrianAttribute;
     PedestrianClassifier(PedestrianConfig &pconf);
     virtual ~PedestrianClassifier();
