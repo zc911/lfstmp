beb349ae1c9cf2fd16d19e3570ec0819299da7c4
JellyC <cjy29088@gmail.com>
2016-08-17 16:57:47 +0800

    crash bug

--- apps/src/services/storage_request.h
+++ apps/src/services/storage_request.h
@@ -4,6 +4,7 @@
 
 #ifndef PROJECT_STORAGE_REQUEST_H
 #define PROJECT_STORAGE_REQUEST_H
+
 #include "witness_bucket.h"
 #include "clients/spring_client.h"
 #include "clients/data_client.h"
@@ -21,63 +22,75 @@ public:
 
     MatrixError storage() {
         VLOG(VLOG_SERVICE) << "========START REQUEST===========" << endl;
+        LOG(INFO)<<"DFE";
 
         MatrixError err;
         shared_ptr<WitnessVehicleObj> wv = WitnessBucket::Instance().Pop();
-        VehicleObj vo;
-        PedestrianObj po;
-        for(int k=0;k<wv->results.size();k++){
-            const WitnessResult &r = wv->results.Mutable(i);
-                for (int i = 0; i < r.vehicles_size(); i++) {
-                    Cutboard c = r.vehicles(i).img().cutboard();
-                    Mat roi(wv->imgs[k]->payload()->data(), Rect(c.x(), c.y(), c.width(), c.height()));
-                    RecVehicle *v=wv->results.Mutable(k)->mutable_vehicles(i);
-                    vector<uchar> data(roi.datastart, roi.dataend);
-                    string imgdata = Base64::Encode(data);
-                    v->mutable_img()->mutable_img()->set_bindata(imgdata);
-                    vo.mutable_vehicle()->AddAllocated(v);
 
-                    v->mutable_metadata()->CopyFrom(srcMetadatas[k]);
-                    v->mutable_img()->set_uri(r.image().data().uri());
-                    v->mutable_img()->set_height(wv->imgs[k].rows);
-                    v->mutable_img()->set_width(wv->imgs[k].cols);
-                }
+        for (int k = 0; k < wv->results.size(); k++) {
+            VehicleObj vo;
+            PedestrianObj po;
+            const WitnessResult &r = wv->results.Get(k);
+            for (int i = 0; i < r.vehicles_size(); i++) {
+                Cutboard c = r.vehicles(i).img().cutboard();
+                Mat roi(wv->imgs[k], Rect(c.x(), c.y(), c.width(), c.height()));
+                RecVehicle *v = wv->results.Mutable(k)->mutable_vehicles(i);
+                vector<uchar> data(roi.datastart, roi.dataend);
+                //string imgdata = Base64::Encode(data);
+                //v->mutable_img()->mutable_img()->set_bindata(imgdata);
+                vo.mutable_vehicle()->AddAllocated(v);
+
+
+            }
             for (int i = 0; i < r.pedestrian_size(); i++) {
                 Cutboard c = r.pedestrian(i).img().cutboard();
-                Mat roi(wv->imgs[k]->payload()->data(), Rect(c.x(), c.y(), c.width(), c.height()));
-                RecPedestrian *v=wv->results.Mutable(k)->mutable_pedestrian(i);
-                vector<uchar> data(roi.datastart, roi.dataend);
-                string imgdata = Base64::Encode(data);
-                v->mutable_img()->mutable_img()->set_bindata(imgdata);
+                Mat roi(wv->imgs[k], Rect(c.x(), c.y(), c.width(), c.height()));
+                RecPedestrian *v = wv->results.Mutable(k)->mutable_pedestrian(i);
                 po.mutable_pedestrian()->AddAllocated(v);
-                v->mutable_metadata()->CopyFrom(srcMetadatas[k]);
-                v->mutable_img()->set_uri(r.image().data().uri());
-                v->mutable_img()->set_height(wv->imgs[k].rows);
-                v->mutable_img()->set_width(wv->imgs[k].cols);
+
+                vector<uchar> data(roi.datastart, roi.dataend);
+               //dee string imgdata = Base64::Encode(data);
+               // v->mutable_img()->mutable_img()->set_bindata(imgdata);
+                po.mutable_pedestrian()->AddAllocated(v);
             }
-        }
-
-        for (int i = 0; i < wv->storages_size(); i++) {
-
-            string address = wv->storages(i).address();
-            if (wv->storages(i).type() == model::POSTGRES) {
-                pool_->enqueue([address, this, vo,po, &err]() {
-                    MatrixError errTmp = this->data_client_.SendBatchData(address, vo,po);
-                    if (errTmp.code() != 0) {
-                        err.set_code(errTmp.code());
-                        err.set_message("send to postgres error");
-                    }
-                });
-            } else if (wv->storages(i).type() == model::KAFKA) {
-                pool_->enqueue([address, this, vo, &err]() {
-                    MatrixError errTmp = spring_client_.IndexVehicle(address, vo);
-                    if (errTmp.code() != 0) {
-                        err.set_code(errTmp.code());
-                        err.set_message("send to kafka error");
-                    }
-                });
-
+            if (r.vehicles_size() > 0) {
+                vo.mutable_metadata()->CopyFrom(wv->srcMetadatas[k]);
+                vo.mutable_img()->set_uri(r.image().data().uri());
+                vo.mutable_img()->set_height(wv->imgs[k].rows);
+                vo.mutable_img()->set_width(wv->imgs[k].cols);
             }
+            if (r.pedestrian_size()) {
+                po.mutable_metadata()->CopyFrom(wv->srcMetadatas[k]);
+                po.mutable_img()->set_uri(r.image().data().uri());
+                po.mutable_img()->set_height(wv->imgs[k].rows);
+                po.mutable_img()->set_width(wv->imgs[k].cols);
+            }
+
+            string s;
+            google::protobuf::TextFormat::PrintToString(vo, &s);
+            VLOG(VLOG_SERVICE) << s << endl;
+          /*  for (int i = 0; i < wv->storages.size(); i++) {
+
+                string address = wv->storages.Get(i).address();
+                if (wv->storages.Get(i).type() == model::POSTGRES) {
+                    pool_->enqueue([address, this, vo, po, &err]() {
+                        MatrixError errTmp = this->data_client_.SendBatchData(address, vo, po);
+                        if (errTmp.code() != 0) {
+                            err.set_code(errTmp.code());
+                            err.set_message("send to postgres error");
+                        }
+                    });
+                } else if (wv->storages.Get(i).type() == model::KAFKA) {
+                    pool_->enqueue([address, this, vo, &err]() {
+                        MatrixError errTmp = spring_client_.IndexVehicle(address, vo);
+                        if (errTmp.code() != 0) {
+                            err.set_code(errTmp.code());
+                            err.set_message("send to kafka error");
+                        }
+                    });
+
+                }
+            }*/
         }
         return err;
     }
