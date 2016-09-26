be985466e5aef448c595cb91ed38de6e45cff9b9
JellyC <cjy29088@gmail.com>
2016-09-26 18:38:22 +0800

    catimage done

--- engine/src/alg/detector/face_detector.h
+++ engine/src/alg/detector/face_detector.h
@@ -13,31 +13,31 @@ using namespace caffe;
 
 namespace dg {
 
-class FaceCaffeDetector:public FaceDetector {
+class FaceCaffeDetector: public FaceDetector {
 public:
     typedef struct {
         bool is_model_encrypt = false;
         int batch_size = 1;
         int gpu_id = 0;
-        int img_scale_max=300;
-        int img_scale_min=240;
-        float scale=1.0f;
+        int img_scale_max = 300;
+        int img_scale_min = 240;
+        float scale = 1.0f;
         float confidence = 0.7;
         bool use_gpu = true;
         string deploy_file;
         string model_file;
-        string layer_name_cls="conv_face_16_cls";
-        string layer_name_reg="conv_face_16_reg";
+        string layer_name_cls = "conv_face_16_cls";
+        string layer_name_reg = "conv_face_16_reg";
     } FaceDetectorConfig;
     FaceCaffeDetector(const FaceDetectorConfig &config);
 
     virtual ~FaceCaffeDetector();
-int Detect(vector<cv::Mat> &img,
-                            vector<vector<Detection> > &detect_results);
+    int Detect(vector<cv::Mat> &img,
+               vector<vector<Detection> > &detect_results);
 private:
-    void Forward(const vector<Mat> &imgs,vector<vector<Detection> > &final_vbbox);
+    void Forward( vector<Mat> &imgs, vector<vector<Detection> > &final_vbbox);
     void GetDetection(vector<Blob<float> *> &outputs,
-                      vector<vector<Detection>> &final_vbbox,vector<cv::Mat> &imgs);
+                      vector<vector<Detection>> &final_vbbox, vector<cv::Mat> &imgs);
     void NMS(vector<Detection> &p, float threshold);
 
 private:
