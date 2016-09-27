7b0e4532a2eaaaa79a6cf41693590c1be5e4d36a
jiajiachen <jiajiachen@deepglint.com>
2016-09-27 16:06:22 +0800

    asdf

--- dgcaffe/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cuda_version=$( nvcc --version | grep release | awk -F , '{print $2}' | awk '{print $2}' )
-echo "Current CUDA version:" $cuda_version
-cp build/lib/* ../lib/dgcaffe/Linux-x86_64/$cuda_version
-cp -r include ../include/dgcaffe
-
-
