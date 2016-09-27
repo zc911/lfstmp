819f6354a2fd314ba10c889d7d031bfde803a768
jiajiachen <jiajiachen@deepglint.com>
2016-09-27 16:06:22 +0800

    asdf

--- dgcaffe/copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp build/lib/* ../apps/lib/dgcaffe/Linux-x86_64/
-cp build/lib/* ../engine/lib/dgcaffe/Linux-x86_64/
-cp build/lib/* ../apps/bin/Debug/libs/
-cp -r include ../engine/include/dgcaffe/
