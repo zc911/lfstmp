86dd3f8229d5c767d15f95251a671be4ea7b0334
zhenchen <zhenchen@deepglint.com>
2016-09-28 19:08:21 +0800

    intergrate gpu rank

--- engine/src/engine/ran_candidates_repo.cpp
+++ engine/src/engine/ran_candidates_repo.cpp
@@ -4,11 +4,14 @@
 
 #include "rank_candidates_repo.h"
 #include <glog/logging.h>
-
+#include "codec/base64.h"
+#include "alg/rank/database.h"
 
 namespace dg {
 
 RankCandidatesRepo::RankCandidatesRepo(const string &repoPath) : repo_path_(repoPath) {
+    database_ = new CDatabase();
+    database_->SetWorkingGPUs(1);
 
 }
 
@@ -28,7 +31,7 @@ void RankCandidatesRepo::loadFromFile(const string &folderPath) {
         boost::char_separator<char> sep(" ");
 
         typedef boost::tokenizer<boost::char_separator<char>> MyTokenizer;
-        string id, feature;
+        string idString, feature;
 
         for (; itr != end; ++itr) {
 
@@ -48,23 +51,74 @@ void RankCandidatesRepo::loadFromFile(const string &folderPath) {
                     MyTokenizer tok(line, sep);
                     MyTokenizer::iterator col = tok.begin();
                     if (col != tok.end()) {
-                        id = *col;
+                        idString = *col;
                         ++col;
                         if (col != tok.end()) {
                             feature = *col;
                         }
                     }
                     RankCandidatesItem item;
-                    item.image_uri = id;
-                    item.feature = feature;
+                    item.image_uri = idString;
+                    Base64::Decode(feature, item.feature);
                     candidates_.push_back(item);
                 }
 
             }
         }
-        cout << "Candidates repo size: " << candidates_.size() << endl;
 
-        cout << "sample: " << candidates_[10000].image_uri << " " << candidates_[10000].feature << endl;
+        cout << "Candidates repo size: " << candidates_.size() << endl;
+        // BASE64 decode bug here, the last item was duplicated
+        int featureLen = candidates_[0].feature.size() - 1;
+        cout << "Feature length is " << featureLen << endl;
+
+        database_->Initialize(candidates_.size(), featureLen);
+
+        int batchSize = 1024;
+        int batchCount = candidates_.size() / batchSize;
+
+        cout << "Batch size: " << batchSize << " and batch count: " << batchCount << endl;
+
+        float *batchFeatures = new float[batchSize * featureLen];
+        int64_t *batchIds = new int64_t[batchSize];
+
+        int64_t id = 0;
+        for (int i = 0; i < batchCount; ++i) {
+            for (int j = 0; j < batchSize; ++j) {
+                id = i * batchSize + j;
+                RankCandidatesItem &item = candidates_[id];
+                memcpy((char *) (batchFeatures + j * featureLen),
+                       (char *) item.feature.data(),
+                       featureLen * sizeof(float));
+                batchIds[j] = id;
+            }
+            database_->AddItems(batchFeatures, batchIds, batchSize);
+        }
+
+
+        cout << "CDATABASE: " << database_->GetItemCount() << endl;
+        int remains = candidates_.size() - id - 1;
+        int remains2 = candidates_.size() % batchSize;
+
+        if (remains > 0) {
+            cout << "Some candidates remains " << remains << "," << remains2 << endl;
+            for (int i = 0; i < remains; ++i) {
+                ++id;
+                RankCandidatesItem &item = candidates_[id];
+                memcpy((char *) batchFeatures + i * featureLen,
+                       (char *) item.feature.data(),
+                       featureLen * sizeof(float));
+                batchIds[i] = id;
+            }
+            database_->AddItems(batchFeatures, batchIds, remains);
+        }
+
+        cout << "CDATABASE: " << database_->GetItemCount() << endl;
+
+        delete[] batchFeatures;
+        delete[] batchIds;
+
+
+
     } else {
         cout << "Invalid folder path: " << folderPath << endl;
     }
