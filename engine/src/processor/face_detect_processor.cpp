1f7ab157084dcdbe2ad58ee6ce3f217c9ed7f445
irene <cjy29088@gmail.com>
2016-10-12 22:37:21 +0800

    align debug

--- engine/src/processor/face_detect_processor.cpp
+++ engine/src/processor/face_detect_processor.cpp
@@ -154,25 +154,25 @@ bool FaceDetectProcessor::process(FrameBatch *frameBatch) {
   for (int i = 0; i < frameIds.size(); ++i) {
     int frameId = frameIds[i];
     Frame *frame = frameBatch->frames()[frameId];
-    if (boxes_in[i].size() == 0)
-      continue;
-    int size = 0, index_id = 0;
+    /* if (boxes_in[i].size() == 0)
+       continue;
+     int size = 0, index_id = 0;
+     for (size_t bbox_id = 0; bbox_id < boxes_in[i].size(); bbox_id++) {
+       Detection detection = boxes_in[i][bbox_id];
+       if (size < detection.box.area()) {
+         index_id = bbox_id;
+         size=detection.box.area();
+       }
+     }
+     Detection detection = boxes_in[i][index_id];
+     Face *face = new Face(base_id_ + 0, detection,
+                           detection.confidence);
+     cv::Mat data = frame->payload()->data();
+     cv::Mat image = data;
+
+     face->set_image(image);
+     frame->put_object(face);*/
     for (size_t bbox_id = 0; bbox_id < boxes_in[i].size(); bbox_id++) {
-      Detection detection = boxes_in[i][bbox_id];
-      if (size < detection.box.area()) {
-        index_id = bbox_id;
-      }
-    }
-    Detection detection = boxes_in[i][index_id];
-    Face *face = new Face(base_id_ + index_id, detection,
-                          detection.confidence);
-    cv::Mat data = frame->payload()->data();
-    cv::Mat image = data;
-    // string name = to_string(bbox_id)+to_string(frameId) + "face.jpg";
-    // imwrite(name, image);
-    face->set_image(image);
-    frame->put_object(face);
-    /*for (size_t bbox_id = 0; bbox_id < boxes_in[i].size(); bbox_id++) {
       Detection detection = boxes_in[i][bbox_id];
       Face *face = new Face(base_id_ + bbox_id, detection,
                             detection.confidence);
@@ -182,7 +182,7 @@ bool FaceDetectProcessor::process(FrameBatch *frameBatch) {
       // imwrite(name, image);
       face->set_image(image);
       frame->put_object(face);
-    }*/
+    }
   }
   return true;
 }
