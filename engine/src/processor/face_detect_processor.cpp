b6fe27ca32c20eb7e085af70cc1d8a50e6b6285e
irene <cjy29088@gmail.com>
2016-10-15 16:14:50 +0800

    set debug flag

--- engine/src/processor/face_detect_processor.cpp
+++ engine/src/processor/face_detect_processor.cpp
@@ -158,6 +158,7 @@ bool FaceDetectProcessor::process(FrameBatch *frameBatch) {
     int frameId = frameIds[i];
     Frame *frame = frameBatch->frames()[frameId];
     sort(boxes_in[i].begin(),boxes_in[i].end(),BoxCmp);
+
     /* if (boxes_in[i].size() == 0)
        continue;
      int size = 0, index_id = 0;
@@ -182,8 +183,8 @@ bool FaceDetectProcessor::process(FrameBatch *frameBatch) {
                             detection.confidence);
       cv::Mat data = frame->payload()->data();
       cv::Mat image = data;
-      // string name = to_string(bbox_id)+to_string(frameId) + "face.jpg";
-      // imwrite(name, image);
+    //   string name = to_string(bbox_id)+to_string(frameId) + "face.jpg";
+     //  imwrite(name, image);
       face->set_image(image);
       frame->put_object(face);
     }
