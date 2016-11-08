9da7d136efaa8444fa20ad826a62d3507b897825
zhenchen <zhenchen@deepglint.com>
2016-11-08 17:20:28 +0800

    format code

--- engine/src/processor/face_detect_processor.h
+++ engine/src/processor/face_detect_processor.h
@@ -20,46 +20,47 @@
 
 namespace dg {
 typedef struct {
-	bool is_model_encrypt = false;
-	int batch_size = 1;
-	int gpu_id = 0;
-	int img_scale_max = 300;
-	int img_scale_min = 240;
-	float scale = 1.0f;
-	float confidence = 0.7;
-	bool use_gpu = true;
+    bool is_model_encrypt = false;
+    int batch_size = 1;
+    int gpu_id = 0;
+    int img_scale_max = 300;
+    int img_scale_min = 240;
+    float scale = 1.0f;
+    float confidence = 0.7;
+    bool use_gpu = true;
 
 
-	string deploy_file;
-	string model_file;
+    string deploy_file;
+    string model_file;
 } FaceDetectorConfig;
 class FaceDetectProcessor: public Processor {
 
-public:
-	enum {DlibMethod = 0, RpnMethod = 1, SsdMethod = 2,FcnMethod=3};
+ public:
+    enum { DlibMethod = 0, RpnMethod = 1, SsdMethod = 2, FcnMethod = 3 };
 
 
-	//FaceDetectProcessor(FaceDetectorConfig config, int method);
-	FaceDetectProcessor(FaceDetectorConfig config, int method);
+    //FaceDetectProcessor(FaceDetectorConfig config, int method);
+    FaceDetectProcessor(FaceDetectorConfig config, int method);
 
-	virtual ~FaceDetectProcessor();
+    virtual ~FaceDetectProcessor();
 
-protected:
-	virtual bool process(Frame *frame);
-	virtual bool process(FrameBatch *frameBatch);
+ protected:
+    virtual bool process(Frame *frame);
+    virtual bool process(FrameBatch *frameBatch);
 
-	virtual bool RecordFeaturePerformance();
-	virtual bool beforeUpdate(FrameBatch *frameBatch);
-	int DetectResult2Detection(const vector<DGFace::DetectResult> &detect_result, vector< vector<Detection> > &detections);
-	void enlarge_box(vector<vector<Detection>> boxes, vector<vector<Rect>> &enlarge_boxes) ;
-private:
-	DGFace::Detector *detector_ = NULL;
-	//FaceDetector *detector_ = NULL;
-	int base_id_;
-	vector<Mat> imgs_;
-	vector<Object *> objs_;
+    virtual bool RecordFeaturePerformance();
+    virtual bool beforeUpdate(FrameBatch *frameBatch);
+    int DetectResult2Detection
+        (const vector<DGFace::DetectResult> &detect_result, vector<vector<Detection> > &detections);
+    void enlarge_box(vector<vector<Detection>> boxes, vector<vector<Rect>> &enlarge_boxes);
+ private:
+    DGFace::Detector *detector_ = NULL;
+    //FaceDetector *detector_ = NULL;
+    int base_id_;
+    vector<Mat> imgs_;
+    vector<Object *> objs_;
 
-	string detect_type_ = "";
+    string detect_type_ = "";
 };
 
 } /* namespace dg */
