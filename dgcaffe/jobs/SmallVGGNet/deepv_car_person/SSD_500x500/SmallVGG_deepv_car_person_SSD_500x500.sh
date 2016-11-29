7b3095b1b669cf0f0c5976537ce71653e7e6c909
zhenchen <zhenchen@deepglint.com>
2016-11-29 14:07:57 +0800

    merge face and vehicle 1st stage

--- dgcaffe/jobs/SmallVGGNet/deepv_car_person/SSD_500x500/SmallVGG_deepv_car_person_SSD_500x500.sh
+++
@@ -1,4 +0,0 @@
-cd /mnt/data1/zdb/work/caffe_for_frcnn/gaoyuan/caffe
-./build/tools/caffe train \
---solver="models/SmallVGGNet/deepv_car_person/SSD_500x500/solver.prototxt" \
---gpu 0,1,2,3 2>&1 | tee jobs/SmallVGGNet/deepv_car_person/SSD_500x500/SmallVGG_deepv_car_person_SSD_500x500.log
