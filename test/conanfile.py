de38b5b13edab3fa82b0dd490c28a26302e09915
dell <dell@ubuntu.ubuntu>
2016-11-29 10:45:53 +0800

    unpdate so

---
+++ test/conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile, CMake
+
+class DGFace(ConanFile):
+	name = "DGFace"
+	version = "0.1.0"
+	setting = "os", "compiler", "build_type", "arch"
+	exports = "*"
+
+def package(self):
+	self.copy("*", dst="include", src="include")
+	self.copy("*", dst="lib", src="lib")
+
+def package_info(self):
+	self.cpp_info.libs = [facesdk]
