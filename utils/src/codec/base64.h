365b7b558ec4e6135a2f366922ce42575dabf179
zhenchen <zhenchen@deepglint.com>
2016-11-30 16:24:47 +0800

    revert base64 decode because of bug

--- utils/src/codec/base64.h
+++ utils/src/codec/base64.h
@@ -40,11 +40,11 @@ typedef struct
 static int base64_decode_value(char value_in)
 {
     static const char decoding[] =
-    { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1,
-            -2, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1,
-            26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-            43, 44, 45, 46, 47, 48, 49, 50, 51 };
+        { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1,
+          -2, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+          15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1,
+          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+          43, 44, 45, 46, 47, 48, 49, 50, 51 };
     static const char decoding_size = sizeof(decoding);
     value_in -= 43;
     if (value_in < 0 || value_in >= decoding_size)
@@ -59,7 +59,7 @@ static void base64_init_decodestate(base64_decodestate* state_in)
 }
 
 static int base64_decode_block(const char* code_in, const int length_in,
-        char* plaintext_out, base64_decodestate* state_in)
+                               char* plaintext_out, base64_decodestate* state_in)
 {
     const char* codechar = code_in;
     char* plainchar = plaintext_out;
@@ -72,54 +72,54 @@ static int base64_decode_block(const char* code_in, const int length_in,
         while (1)
         {
             case step_a:
-            do
-            {
-                if (codechar == code_in + length_in)
+                do
                 {
-                    state_in->step = step_a;
-                    state_in->plainchar = *plainchar;
-                    return plainchar - plaintext_out;
-                }
-                fragment = (char) base64_decode_value(*codechar++);
-            } while (fragment < 0);
+                    if (codechar == code_in + length_in)
+                    {
+                        state_in->step = step_a;
+                        state_in->plainchar = *plainchar;
+                        return plainchar - plaintext_out;
+                    }
+                    fragment = (char) base64_decode_value(*codechar++);
+                } while (fragment < 0);
             *plainchar = (fragment & 0x03f) << 2;
             case step_b:
-            do
-            {
-                if (codechar == code_in + length_in)
+                do
                 {
-                    state_in->step = step_b;
-                    state_in->plainchar = *plainchar;
-                    return plainchar - plaintext_out;
-                }
-                fragment = (char) base64_decode_value(*codechar++);
-            } while (fragment < 0);
+                    if (codechar == code_in + length_in)
+                    {
+                        state_in->step = step_b;
+                        state_in->plainchar = *plainchar;
+                        return plainchar - plaintext_out;
+                    }
+                    fragment = (char) base64_decode_value(*codechar++);
+                } while (fragment < 0);
             *plainchar++ |= (fragment & 0x030) >> 4;
             *plainchar = (fragment & 0x00f) << 4;
             case step_c:
-            do
-            {
-                if (codechar == code_in + length_in)
+                do
                 {
-                    state_in->step = step_c;
-                    state_in->plainchar = *plainchar;
-                    return plainchar - plaintext_out;
-                }
-                fragment = (char) base64_decode_value(*codechar++);
-            } while (fragment < 0);
+                    if (codechar == code_in + length_in)
+                    {
+                        state_in->step = step_c;
+                        state_in->plainchar = *plainchar;
+                        return plainchar - plaintext_out;
+                    }
+                    fragment = (char) base64_decode_value(*codechar++);
+                } while (fragment < 0);
             *plainchar++ |= (fragment & 0x03c) >> 2;
             *plainchar = (fragment & 0x003) << 6;
             case step_d:
-            do
-            {
-                if (codechar == code_in + length_in)
+                do
                 {
-                    state_in->step = step_d;
-                    state_in->plainchar = *plainchar;
-                    return plainchar - plaintext_out;
-                }
-                fragment = (char) base64_decode_value(*codechar++);
-            } while (fragment < 0);
+                    if (codechar == code_in + length_in)
+                    {
+                        state_in->step = step_d;
+                        state_in->plainchar = *plainchar;
+                        return plainchar - plaintext_out;
+                    }
+                    fragment = (char) base64_decode_value(*codechar++);
+                } while (fragment < 0);
             *plainchar++ |= (fragment & 0x03f);
         }
     }
@@ -138,14 +138,14 @@ static void base64_init_encodestate(base64_encodestate* state_in)
 static char base64_encode_value(char value_in)
 {
     static const char* encoding =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     if (value_in > 63)
         return '=';
     return encoding[(int) value_in];
 }
 
 static int base64_encode_block(const char* plaintext_in, int length_in, char* code_out,
-        base64_encodestate* state_in)
+                               base64_encodestate* state_in)
 {
     const char* plainchar = plaintext_in;
     const char* const plaintextend = plaintext_in + length_in;
@@ -160,34 +160,34 @@ static int base64_encode_block(const char* plaintext_in, int length_in, char* co
         while (1)
         {
             case step_A:
-            if (plainchar == plaintextend)
-            {
-                state_in->result = result;
-                state_in->step = step_A;
-                return codechar - code_out;
-            }
+                if (plainchar == plaintextend)
+                {
+                    state_in->result = result;
+                    state_in->step = step_A;
+                    return codechar - code_out;
+                }
             fragment = *plainchar++;
             result = (fragment & 0x0fc) >> 2;
             *codechar++ = base64_encode_value(result);
             result = (fragment & 0x003) << 4;
             case step_B:
-            if (plainchar == plaintextend)
-            {
-                state_in->result = result;
-                state_in->step = step_B;
-                return codechar - code_out;
-            }
+                if (plainchar == plaintextend)
+                {
+                    state_in->result = result;
+                    state_in->step = step_B;
+                    return codechar - code_out;
+                }
             fragment = *plainchar++;
             result |= (fragment & 0x0f0) >> 4;
             *codechar++ = base64_encode_value(result);
             result = (fragment & 0x00f) << 2;
             case step_C:
-            if (plainchar == plaintextend)
-            {
-                state_in->result = result;
-                state_in->step = step_C;
-                return codechar - code_out;
-            }
+                if (plainchar == plaintextend)
+                {
+                    state_in->result = result;
+                    state_in->step = step_C;
+                    return codechar - code_out;
+                }
             fragment = *plainchar++;
             result |= (fragment & 0x0c0) >> 6;
             *codechar++ = base64_encode_value(result);
@@ -231,7 +231,7 @@ static int base64_encode_blockend(char* code_out, base64_encodestate* state_in)
 
 namespace dg {
 class Base64 {
-public:
+ public:
     static int Encode(char value) {
         return base64_encode_value(value);
     }
@@ -312,11 +312,13 @@ public:
         T temp;
         while (ss_out.good()) {
             ss_out.read((char *) (&temp), sizeof(T));
+            if(!ss_out.good())
+                break;
             result_array.push_back(temp);
         }
     }
 
-private:
+ private:
     static int encodeEnd(char *buffer_in, base64_encodestate *state) {
         return base64_encode_blockend(buffer_in, state);
     }
