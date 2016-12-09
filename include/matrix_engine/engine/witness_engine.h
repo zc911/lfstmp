c9d44a936d4427ed5552d4db33fda208363575e4
zhenchen <zhenchen@deepglint.com>
2016-12-09 16:29:02 +0800

    add detection threshold

--- include/matrix_engine/engine/witness_engine.h
+++ include/matrix_engine/engine/witness_engine.h
@@ -27,6 +27,7 @@ private:
     void init(const Config &config);
     void initFeatureOptions(const Config &config);
     void initGpuMemory(FrameBatch &batch);
+    void formatPipeline(Processor *last, Processor *p);
 
     void recordPerformance();
     Processor *vehicle_processor_;
