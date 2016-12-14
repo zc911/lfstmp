7e7a0a5bf5743f3a148c81ad4170d314d7a6ae62
zhenchen <zhenchen@deepglint.com>
2016-12-14 20:00:20 +0800

    add face model into lfs

--- data/run.sh
+++ data/run.sh
@@ -1,19 +1,3 @@
-wget http://192.168.2.119/matrix/pull_models.tar
-tar xvf pull_models.tar
-cp pull_models/model_encrypt . 
-cp pull_models/model_key.perm .
-for file in `ls 0/`
-do
-    if [ "$file" = "avgface.jpg" -o "$file" = "pedestrian_attribute_tagnames.txt" ];then
-        cp 0/$file 1/
-    elif [ "$file" = "501.dat" ];then
-        cp 0/$file 1/
-    elif [ "$file" = "bitri_threshold.txt" ];then
-        cp 0/$file 1/
-    else
-        ./model_encrypt -i 0/"$file" -o 1/"$file"
-    fi 
-done
-
-rm -rf model_*
-rm -rf pull_models*
+version https://git-lfs.github.com/spec/v1
+oid sha256:65a35d59ed82ddeb283a67e1379bd5b10f7c1d5ac10cf2cc79739835c79f84e6
+size 511
